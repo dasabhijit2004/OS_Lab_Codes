cbec3c8ad4b56cdbcea1487807163c8c0dddfff1
Abhijit Das <150443491+dasabhijit2004@users.noreply.github.com>
2024-08-15 14:35:59 +0530

    Update assignment-1.sh

--- Assignment-1/assignment-1.sh
+++ Assignment-1/assignment-1.sh
@@ -8,8 +8,8 @@ count=0
 for(( i=1; i<=l; i++ ))
 do
   x=`head -n $i test_case.txt | tail -n 1 | cut -d " " -f 1`
-  y=`head -n $i test_case.txt | tail -n 1 | cut -d " " -f 1`
-  z=`head -n $i test_case.txt | tail -n 1 | cut -d " " -f 1`
+  y=`head -n $i test_case.txt | tail -n 1 | cut -d " " -f 2`
+  z=`head -n $i test_case.txt | tail -n 1 | cut -d " " -f 3`
 
   res=`sh GCD/$file $x $y`
   if [ $res == $z ]
@@ -22,8 +22,8 @@ do
 done
 if [[ $flag -eq 1 ]]
 then 
-  echo "$file failed for $count test cases"
+  echo "$file failed for $count test case(s)"
 else
-  echo "$file passed"
+  echo "$file passed for all case(s)"
 fi
 done
